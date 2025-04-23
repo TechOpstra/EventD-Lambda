a354c03168547620110d6fe7dbb915c681b4fed9
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 13:08:11 +0530

    Update main.tf

--- Infra/modules/s3-bucket/main.tf
+++ Infra/modules/s3-bucket/main.tf
@@ -1,15 +1,23 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
+
 resource "aws_s3_bucket" "upload_bucket" {
   bucket = "image-upload-bucket-${random_id.bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket_acl" "upload_bucket_acl" {
+  bucket = aws_s3_bucket.upload_bucket.bucket
   acl    = "private"
 }
 
 resource "aws_s3_bucket" "processed_bucket" {
   bucket = "processed-image-bucket-${random_id.bucket_suffix.hex}"
-  acl    = "private"
 }
 
-resource "random_id" "bucket_suffix" {
-  byte_length = 8
+resource "aws_s3_bucket_acl" "processed_bucket_acl" {
+  bucket = aws_s3_bucket.processed_bucket.bucket
+  acl    = "private"
 }
 
 
