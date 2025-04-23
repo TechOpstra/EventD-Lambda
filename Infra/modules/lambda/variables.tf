6c5f14460b07b7b814884c844abb5e89a48c0930
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 15:01:19 +0530

    Update variables.tf

--- Infra/modules/lambda/variables.tf
+++ Infra/modules/lambda/variables.tf
@@ -23,3 +23,8 @@ variable "processed_bucket_name" {
   description = "The name of the processed S3 bucket"
   type        = string
 }
+
+variable "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  type        = string
+}
