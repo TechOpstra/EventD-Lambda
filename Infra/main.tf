6e1348337b72cd115076ae60beb0e6d2b693807f
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 12:53:54 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -3,14 +3,15 @@ module "s3_buckets" {
 }
 
 module "lambda_function" {
-  source             = "./modules/lambda"
-  bucket_name        = module.s3_buckets.upload_bucket_name
-  lambda_function_name = "ImageProcessingLambda"  
+  source                  = "./modules/lambda"
+  bucket_name             = module.s3_buckets.upload_bucket_name
+  lambda_function_name    = "ImageProcessingLambda"
+  lambda_execution_role_arn = module.iam_roles.lambda_execution_role_arn  # Pass ARN here
 }
 
 module "iam_roles" {
   source             = "./modules/iam"
-  bucket_name        = module.s3_buckets.upload_bucket_name 
+  bucket_name        = module.s3_buckets.upload_bucket_name  # Pass bucket_name to IAM module
 }
 
 module "xray" {
