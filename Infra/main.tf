25a5b332007e113ff8907cb7a19a3e20f4c00787
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 11:40:58 +0530

    Update main.tf

---
+++ Infra/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred AWS region
+}
+
+module "s3_buckets" {
+  source = "./modules/s3"
+}
+
+module "lambda_function" {
+  source = "./modules/lambda"
+  bucket_name = module.s3_buckets.upload_bucket_name
+}
+
+module "iam_roles" {
+  source = "./modules/iam"
+  lambda_role_policy_arn = module.lambda_function.lambda_role_arn
+}
+
+module "xray" {
+  source = "./modules/xray"
+  lambda_function_name = module.lambda_function.lambda_function_name
+}
+
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+  lambda_function_name = module.lambda_function.lambda_function_name
+}
