fd8e4b1bf4b1f81ddf2db528f3051fad4cd65996
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 13:33:47 +0530

    Update main.tf

--- Infra/modules/s3-bucket/main.tf
+++ Infra/modules/s3-bucket/main.tf
@@ -6,20 +6,10 @@ resource "aws_s3_bucket" "upload_bucket" {
   bucket = "image-upload-bucket-${random_id.bucket_suffix.hex}"
 }
 
-resource "aws_s3_bucket_acl" "upload_bucket_acl" {
-  bucket = aws_s3_bucket.upload_bucket.bucket
-  acl    = "private"
-}
 
 resource "aws_s3_bucket" "processed_bucket" {
   bucket = "processed-image-bucket-${random_id.bucket_suffix.hex}"
 }
-
-resource "aws_s3_bucket_acl" "processed_bucket_acl" {
-  bucket = aws_s3_bucket.processed_bucket.bucket
-  acl    = "private"
-}
-
 
 
 output "upload_bucket_name" {
