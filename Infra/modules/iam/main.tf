ece10099c58cc88b15b370b39c8458c779255858
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 13:12:43 +0530

    Update main.tf

--- Infra/modules/iam/main.tf
+++ Infra/modules/iam/main.tf
@@ -28,8 +28,8 @@ data "aws_iam_policy_document" "lambda_policy" {
   statement {
     actions   = ["s3:GetObject", "s3:PutObject"]
     resources = [
-      "arn:aws:s3:::${var.bucket_name}/*",
-      "arn:aws:s3:::${module.s3_buckets.processed_bucket_name}/*"
+      "arn:aws:s3:::${var.upload_bucket_name}/*",
+      "arn:aws:s3:::${var.processed_bucket_name}/*"
     ]
   }
 }
