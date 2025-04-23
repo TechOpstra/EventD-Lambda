cbb0077e19844f931b9d6be61372ec1afa292449
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 12:35:30 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,24 +1,24 @@
-
 module "s3_buckets" {
   source = "./modules/s3-bucket"
 }
 
 module "lambda_function" {
-  source = "./modules/lambda"
-  bucket_name = module.s3_buckets.upload_bucket_name
+  source             = "./modules/lambda"
+  bucket_name        = module.s3_buckets.upload_bucket_name
+  lambda_function_name = "ImageProcessingLambda"  # Define the Lambda function name here
 }
 
 module "iam_roles" {
-  source = "./modules/iam"
+  source                = "./modules/iam"
   lambda_role_policy_arn = module.lambda_function.lambda_role_arn
 }
 
 module "xray" {
-  source = "./modules/xray"
+  source               = "./modules/xray"
   lambda_function_name = module.lambda_function.lambda_function_name
 }
 
 module "cloudwatch" {
-  source = "./modules/cloudwatch"
+  source               = "./modules/cloudwatch"
   lambda_function_name = module.lambda_function.lambda_function_name
 }
