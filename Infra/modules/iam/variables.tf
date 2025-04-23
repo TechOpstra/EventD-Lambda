86cb983f91816397904056e1eea0b3af940fab2f
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-23 13:10:04 +0530

    Update variables.tf

--- Infra/modules/iam/variables.tf
+++ Infra/modules/iam/variables.tf
@@ -1,4 +1,9 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
+variable "upload_bucket_name" {
+  description = "The name of the upload S3 bucket"
+  type        = string
+}
+
+variable "processed_bucket_name" {
+  description = "The name of the processed S3 bucket"
   type        = string
 }
