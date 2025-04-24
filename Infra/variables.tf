7d54db525ba78ee157f3e3e38fd6969d415e21dd
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-24 08:08:59 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -3,14 +3,3 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "upload_bucket_name" {
-  description = "Name of the S3 bucket for image uploads"
-  type        = string
-}
-
-variable "processed_bucket_name" {
-  description = "Name of the S3 bucket for processed images"
-  type        = string
-}
-
