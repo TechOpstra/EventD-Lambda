85016c03e7e84c1c33b3f8e3db6a52c205c2055d
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-08 22:08:51 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.0"
-  backend "s3" {
-    bucket         = "techopsbucket123"
-    key            = "vpcec2/terraform.tfstatevent"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "demo_table123"
-  }
+  required_version = ">= 1.10.0"
+  backend "s3" {
+    bucket       = "techopsbucket123"
+    key          = "vpcec2/terraform.tfstateimageresize"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
